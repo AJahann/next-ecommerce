6bcf49e792d0d983f50dfeef146b6e34f73a93d2
αѕнкαη <as6kan.developer@gmail.com>
2024-11-18 12:32:30 +0330

    add products image

--- src/components/CategoryList.tsx
+++ src/components/CategoryList.tsx
@@ -1,22 +1,23 @@
-import type { CategoryDocument } from '@/models/Category';
+import type { CategoryBase } from '@/models/Category';
 
 import CategoryRepository from '@/repositories/CategoryRepository';
 import Image from 'next/image';
 import Link from 'next/link';
 
 const CategoryList = async () => {
-  const categories: CategoryDocument[] =
+  const categories: CategoryBase[] =
     await CategoryRepository.getAllCategories();
 
   return (
     <div className="scrollbar-hide overflow-x-scroll px-4">
       <div className="flex gap-4 md:gap-8">
-        {categories.map((category) => {
+        {categories.map((category, index) => {
           return (
             <Link
               className="w-full shrink-0 sm:w-1/2 lg:w-1/4 xl:w-1/6"
               href={`/list?category=${category.name.replaceAll(' ', '-')}`}
-              key={category.id}
+              // eslint-disable-next-line @eslint-react/no-array-index-key
+              key={index}
             >
               <div className="relative h-96 w-full bg-slate-100">
                 <Image
@@ -24,7 +25,7 @@ const CategoryList = async () => {
                   alt=""
                   className="object-cover"
                   sizes="20vw"
-                  src={'/woman.png'}
+                  src={category.image.url}
                 />
               </div>
               <h1 className="mt-8 text-xl font-light tracking-wide">
